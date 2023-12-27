f199805d6b19e20148d57e7cd78250f94950b443
Diana-Llanos <https://github.com/login>
2023-12-27 15:50:43 -0500

    estados de la aplicacion

--- main.js
+++ main.js
@@ -1,14 +1,24 @@
 const { app, BrowserWindow } = require('electron')
 
-const createWindow = () => {
+const createWindow = () => {                      //funcion createWindows, define ancho y alto de la ventana
   const win = new BrowserWindow({
     width: 800,
     height: 600
   })
 
-  win.loadFile('index.html')
+  win.loadFile('index.html')                     //indicamos el archivo html que se va a cargar ya sea proceso de renderizado o pagina web
 }
 
-app.whenReady().then(() => {
+app.whenReady().then(() => {                    //iniciamos o cargarmos ventana cuando todo este listo
   createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length ===0){  
+      createWindow ()
+  }
+})
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
 })
